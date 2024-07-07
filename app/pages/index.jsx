f271ee6758e815a9f51cf5e39b0ac0a5cc1535b0
sano <sano@ueda.info.waseda.ac.jp>
2024-07-07 22:12:35 +0900

    add prooftree.js

--- app/pages/index.jsx
+++ app/pages/index.jsx
@@ -350,7 +350,7 @@ export default class PreviewPage extends React.Component {
           <script type="text/javascript" src="/_static/raphael@2.3.0.min.js"></script>
           <script type="text/javascript" src="/_static/flowchart@1.13.0.min.js"></script>
           <script type="text/javascript" src="/_static/viz.js"></script>
-          <script type="text/javascript" src="/_static/prooftree.js"></script>
+          {/* <script type="text/javascript" src="/_static/prooftree.js"></script> */}
           <script type="text/javascript" src="/_static/full.render.js"></script>
         </Head>
         <main data-theme={this.state.theme}>
